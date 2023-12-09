3a2e5fc9fef1e6b2657839c11f91727eb2d671f5
htiennv <tien.nv291997@gmail.com>
2023-12-09 14:27:44 +0700

    Move bin to test.

--- crates/queue/src/lib.rs
+++ crates/queue/src/lib.rs
@@ -27,4 +27,39 @@ impl<T> Queue<T> {
         let value = s.pop_front()?;
         Some(value)
     }
+
+    pub fn len(&self) -> usize {
+        self.items.lock().unwrap().len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use crate::Queue;
+
+    #[test]
+    fn test_push_pop() {
+        let queue = Arc::new(Queue::new());
+
+        let q = queue.clone();
+        // Create threads to push and pop elements from the queue
+        let thread1 = std::thread::spawn(move || {
+            q.push(1);
+            q.push(2);
+            q.push(3);
+        });
+        let q = queue.clone();
+        let thread2 = std::thread::spawn(move || loop {
+            if q.len() == 0 {
+                break;
+            }
+            let item = q.pop();
+            println!("Popped item: {:?}", item);
+        });
+    
+        thread1.join().unwrap();
+        thread2.join().unwrap();
+    }
 }
